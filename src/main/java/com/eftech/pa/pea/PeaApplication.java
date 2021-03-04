d7dc54dffaa5c286b1c366bd78461c4724d33fe4
Fangzhou Yuan <fyuan@arccorp.com>
2021-03-03 20:39:06 -0500

    Add  customized swagger docs to Category API

--- src/main/java/com/eftech/pa/pea/PeaApplication.java
+++ src/main/java/com/eftech/pa/pea/PeaApplication.java
@@ -7,6 +7,10 @@ import com.eftech.pa.pea.impl.persistent.*;
 import com.eftech.pa.pea.utils.Generator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.math.BigDecimal;
@@ -59,6 +63,15 @@ public class PeaApplication {
 		UserDao userDao = new HibernateUserDaoImpl();
 		userDao.save(user);
 		 **/
+	}
+
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.eftech.pa.pea"))
+				.build();
 	}
 
 }
--- src/main/java/com/eftech/pa/pea/api/CategoryExtService.java
+++ src/main/java/com/eftech/pa/pea/api/CategoryExtService.java
@@ -2,6 +2,10 @@ package com.eftech.pa.pea.api;
 
 import com.eftech.pa.pea.dto.CategoryDTO;
 import com.eftech.pa.pea.exception.ApiException;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,10 @@ public interface CategoryExtService {
      * @return a list of category DTOs
      */
     @RequestMapping(path = "/all", method = RequestMethod.GET)
+    @ApiOperation(value = "Get all categories",
+            notes = "Return a list of all existing categories",
+            response = CategoryDTO.class,
+            responseContainer = "List")
     List<CategoryDTO> getCategories();
 
     /**
@@ -25,7 +33,15 @@ public interface CategoryExtService {
      * @throws ApiException exception to throw
      */
     @RequestMapping(method = RequestMethod.POST)
-    CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) throws ApiException;
+    @ApiOperation(value = "Create category",
+            notes = "Create a new category ",
+            response = CategoryDTO.class)
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "Invalid request body"),
+            @ApiResponse(code = 409, message = "Duplicate category")
+    })
+    CategoryDTO createCategory(@ApiParam(name = "Request body", value = "Category information for a new category to be created", required = true)
+                               @RequestBody CategoryDTO categoryDTO) throws ApiException;
 
     /**
      * Retrieve category with specified id
@@ -34,7 +50,14 @@ public interface CategoryExtService {
      * @throws ApiException exception to throw
      */
     @RequestMapping(path = "/{categoryId}", method = RequestMethod.GET)
-    CategoryDTO getCategory(@PathVariable("categoryId") String categoryId) throws ApiException;
+    @ApiOperation(value = "Get category",
+            notes = "Get an existing category by providing category id",
+            response = CategoryDTO.class)
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Category not found")
+    })
+    CategoryDTO getCategory( @ApiParam(value = "ID for target category", required = true)
+                             @PathVariable("categoryId") String categoryId) throws ApiException;
 
     /**
      * Update an existing category
@@ -44,7 +67,17 @@ public interface CategoryExtService {
      * @throws ApiException exception to throw
      */
     @RequestMapping(path = "/{categoryId}", method = RequestMethod.PUT)
-    CategoryDTO updateCategory(@PathVariable("categoryId") String categoryId, @RequestBody CategoryDTO categoryDTO) throws ApiException;
+    @ApiOperation(value = "Update category",
+            notes = "Update an existing category",
+            response = CategoryDTO.class)
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "Invalid request body"),
+            @ApiResponse(code = 409, message = "Duplicate category")
+    })
+    CategoryDTO updateCategory(@ApiParam(value = "ID for target category", required = true)
+                               @PathVariable("categoryId") String categoryId,
+                               @ApiParam(name = "Request body", value = "Category information for an existing category to be updated to", required = true)
+                               @RequestBody CategoryDTO categoryDTO) throws ApiException;
 
     /**
      * Delete category with specified id
@@ -52,6 +85,12 @@ public interface CategoryExtService {
      * @throws ApiException exception to throw
      */
     @RequestMapping(path = "/{categoryId}", method = RequestMethod.DELETE)
-    void deleteCategory(@PathVariable("categoryId") String categoryId) throws ApiException;
+    @ApiOperation(value = "Delete category",
+            notes = "Delete an existing category by providing category id")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Category not found")
+    })
+    void deleteCategory(@ApiParam(value = "ID for target category", required = true)
+                        @PathVariable("categoryId") String categoryId) throws ApiException;
 
 }
